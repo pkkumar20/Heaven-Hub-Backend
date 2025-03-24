507a195b331bbf554db06d364b2fbae8552ac4e7
Priyanshu Kumar <bgmiunban>
2025-03-24 19:29:46 +0530

    backend Files updated

--- app.js
+++ app.js
@@ -35,7 +35,9 @@ const sessionOptions = {
   cookie: {
     expires: Date.now() + 7 * 24 * 60 * 60 + 1000,
     maxAge: 7 * 24 * 60 * 60 + 1000,
-    httpOnly:true,
+    httpOnly: true,
+    sameSite: "none",
+    secure: true
   }
 }
 const server = http.createServer(app);
